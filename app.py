e7a2aba162732500a2201d06dd46987292f8b8c7
An7542 <agarciapsanchez@gmail.com>
2026-05-16 14:42:04 -0600

    lol

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template  # Agrega render_template aquí
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Sentinel AI Running"
+    return render_template("index.html")  # Cambia esto para que cargue el HTML
 
 if __name__ == "__main__":
     app.run(debug=True)
