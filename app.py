5f573acd6c41eb27804d245c6712dc5bae97d695
Emiliano Mayorga Solórzano <emilianomayorgasolorzano@Emilianos-MacBook-Pro-2.local>
2026-05-16 17:05:28 -0600

    avance intent engine y transcripcion

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-<<<<<<< Updated upstream
-from flask import Flask
-from faster_whisper import WhisperModel
+from flask import Flask, request, jsonify
+from transcriber import transcribe_audio
 
 app = Flask(__name__)
-
-model = WhisperModel("tiny", device="cpu", compute_type="int8")
 
 @app.route("/")
 def home():
@@ -12,28 +9,15 @@ def home():
 
 @app.route("/test")
 def test():
-
-    segments, info = model.transcribe("audio_capture.wav", language="es")
-
-    texto = ""
-
-    for segment in segments:
-        texto += segment.text
-
-    return texto
-
-if __name__ == "__main__":
-    app.run(debug=True)
-=======
-from flask import Flask, request, jsonify
-from transcriber import transcribe_audio
-
-app = Flask(__name__)
+    return "Ruta test funcionando"
 
 @app.route("/transcribe", methods=["POST"])
 def transcribe():
+
     audio = request.files["audio"]
+
     path = "uploads/audio.wav"
+
     audio.save(path)
 
     texto = transcribe_audio(path)
@@ -41,4 +25,6 @@ def transcribe():
     return jsonify({
         "text": texto
     })
->>>>>>> Stashed changes
+
+if __name__ == "__main__":
+    app.run(debug=True)
